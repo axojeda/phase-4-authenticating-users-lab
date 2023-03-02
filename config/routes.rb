98fa719a120148e5ca82b210c3130062b0607bfe
Aura <ax.pink@gmail.com>
2023-03-02 17:00:25 -0500

    commit tree ec165e96d668b94ae6326a8d02e57ff0074d89ea

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
+post '/login', to: 'session#create'
+delete '/logout', to: 'session#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
